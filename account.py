29977a460efeb7f54529a3bfb64ac40be67595d2
Cody McCarty <codyjmccarty@gmail.com>
2021-06-03 10:34:09 -0500

    1.1 added account and owners

--- account.py
+++ account.py
@@ -3,6 +3,8 @@
 class Account():
     
     def __init__(self, account_id, initial_balance):
+        if initial_balance < 0:
+            raise Exception ("Must enter a positive balance")
         self.account_id = account_id
         self.current_balance = initial_balance
 
@@ -14,10 +16,15 @@ class Account():
         self.show_balance()
 
     def withdraw(self, ammount):
-        self.current_balance -= ammount
+        future_ammount = self.current_balance - ammount
+        if future_ammount < 0:
+            raise Exception (f"Insufenciant funds.\n Your current balance is $ {self.current_balance}, and you can't withdraw $ {ammount}  " )
+        self.current_balance = future_ammount
         self.show_balance()
 
 
 
 ca = Account(7080, 500)
 ca.show_balance()
+ca.withdraw(200)
+ca.deposit(3500)
---
+++ owner.py
@@ -0,0 +1,15 @@
+# owner.py
+
+class Owner():
+
+    def __init__(self, account_info, owner = None):
+        self.account_info = account_info
+        self.owner = owner
+
+    def set_account_owner(self, owner):
+        self.owner = owner
+
+
+
+
+
